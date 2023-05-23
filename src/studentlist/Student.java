5b4f7178314fd06f28997abbb0a51ed614ed48a0
Priyal007 <133786818+Priyal007@users.noreply.github.com>
2023-05-23 09:32:29 -0400

    Update Student.java
    comment added on github

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -4,7 +4,7 @@ package studentlist;
 public class Student {
     private String sname;
     private String sId; 
-
+//comment added on github to show fetch+merge
     public Student(String sname, String sId) {
         this.sname = sname;
         this.sId = sId;
